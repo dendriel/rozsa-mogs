5aa040e75c61638c55f73454120ff17a1b232222
Vitor Rozsa <vitor.rozsa@hotmail.com>
2025-03-14 18:18:03 -0300

    Update build settings

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,16 @@
 // Contents of the file /rollup.config.mjs
 import typescript from '@rollup/plugin-typescript';
 import dts from "rollup-plugin-dts";
+
 const config = [
     {
         input: 'dist/compiled/index.js',
         output: {
             file: 'dist/compiled/rozsa-mogs.js',
-            format: 'cjs',
+            // format: 'cjs',
             sourcemap: true
         },
-        external: ['socket.io', 'socket.io-client'],
-        inlineDynamicImports: true, // ✅ Inline all dynamic imports
+        external: ['socket.io-client'],
         plugins: [typescript()]
     }, {
         input: 'dist/compiled/index.d.ts',
